f073839feea7f4767de188cd932f09b67f274f01
Dan Sonnenburg <dan.sonnenburg@organicvalley.com>
2022-06-01 14:28:17 -0500

    added param block example

--- Section_10_Examples-InputAndOutput.ps1
+++ Section_10_Examples-InputAndOutput.ps1
@@ -2,17 +2,32 @@
 # How to use Read-Host cmdlet
 Read-Host "Enter a computer name"
 
-# Example 10.2
+# Example 10.2 Parameter Block
+Function Do-SomethingUseful {
+    param(
+        [switch]$SomethingUseful
+    )
+    If ($SomethingUseful) {
+        Write-Output "This was really useful."
+    }
+    Else {
+        Write-Output "This was not useful."
+    }
+}
+Do-SomethingUseful -SomethingUseful # This switch parameter bounds the output.
+Do-SomethingUseful # This time the switch was not used and therefore didn't do anything useful.
+
+# Example 10.3
 # How to use Write-Host (ignores the pipeline)
 Write-Host "Server01" -ForegroundColor Yellow -BackgroundColor Magenta
 
-# Example 10.3
+# Example 10.4
 # How to use write-output (uses the pipeline by default)
 Write-Output "Hello"
 
 # Write-Output can filter output in the pipeline example 1
-Write-Output "Hello" | Where-Object {$_.length -gt 10} # returns nothing, because it does not meet criteria
+Write-Output "Hello" | Where-Object { $_.length -gt 10 } # returns nothing, because it does not meet criteria
 
 # Write-Output with filtered output in pipeline example 2
-[int[]]$integers = 1,2,3,5,10,20,30,40,50
-Write-Output $integers | Where-Object{$_ -gt 10}
+[int[]]$integers = 1, 2, 3, 5, 10, 20, 30, 40, 50
+Write-Output $integers | Where-Object { $_ -gt 10 }
