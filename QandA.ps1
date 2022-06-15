53efc9558a801dc4587519210c7051829b2cc9ec
Dan Sonnenburg <dan.sonnenburg@organicvalley.com>
2022-06-15 10:18:32 -0500

    added merge file example

--- QandA.ps1
+++ QandA.ps1
@@ -21,3 +21,12 @@ Select-Worksheet -ExcelPackage $excel -WorksheetName "Sheet1"
 
 Set-ExcelRow -ExcelPackage $excel -value "test3" -verbose
 Close-ExcelPackage -ExcelPackage $excel
+
+#
+
+#merge multiple text files into a single file
+$outputdir = Resolve-Path .
+Remove-Item -path "$outputdir\CombinedFiles.ps1" -ErrorAction Ignore
+foreach ($file in (Get-ChildItem -Path "$outputdir\*.ps1" | Sort-Object Name)){
+    Get-Content $file | out-file "$outputdir\CombinedFiles.ps1" -Append
+}
