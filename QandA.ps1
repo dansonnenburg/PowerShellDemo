ad9986c3de0c7be4509851162d369752ae943316
Dan Sonnenburg <dan.sonnenburg@organicvalley.com>
2022-06-16 07:54:49 -0500

    added demos in Q and A script

--- QandA.ps1
+++ QandA.ps1
@@ -15,7 +15,7 @@ Get-Command -Module ImportExcel
 # Execute some commands in Excel
 Get-ExcelSheetInfo -Path ".\excel_demo.xlsx"
 
-$path = Resolve-Path ".\powershelldemo\excel_demo.xlsx"
+$path = Resolve-Path ".\excel_demo.xlsx"
 $excel = Open-ExcelPackage -Path $path
 Select-Worksheet -ExcelPackage $excel -WorksheetName "Sheet1" 
 
@@ -30,3 +30,45 @@ Remove-Item -path "$outputdir\CombinedFiles.ps1" -ErrorAction Ignore
 foreach ($file in (Get-ChildItem -Path "$outputdir\*.ps1" | Sort-Object Name)){
     Get-Content $file | out-file "$outputdir\CombinedFiles.ps1" -Append
 }
+
+#Jeremy - Formatting network addresses
+[System.Net.IPAddress]$IPAddr = "8.8.8.8"
+$IPAddr | Get-Member
+[System.Net.IPAddress]$IPAddr = "600.40.10.1"
+
+[System.Net.NetworkInformation.PhysicalAddress]$macaddr = "D4-54-8B-AD-5C-9B"
+$macaddr | Get-Member
+$macaddr.ToString() -Split '(..)' -ne '' -join ':'
+[System.Net.NetworkInformation.PhysicalAddress]$macaddr = "D4:54:8B:AD:5C:9B"
+[System.Net.NetworkInformation.PhysicalAddress]$macaddr = "D4548BAD5C9B"
+[System.Net.NetworkInformation.PhysicalAddress]$macaddr = "Z4-54-8B-AD-5C-9B"
+
+$macaddr.GetType().Name
+
+#Tom - Outputting multiple variables or data from function
+function Format-SomeInput {
+    param(
+        $InputObject
+    )
+
+    if ($InputObject -is [String]){
+        $OutputInfo = "StringValue = $InputObject"
+    }
+    elseif ($InputObject -is [int]) {
+        $OutputInfo = "IntValue = $InputObject"
+    }
+    else {
+        $OutputInfo = "$($InputObject.GetType().Name)Value = $InputObject"
+    }
+
+    Write-Output [PSCustomObject]@{
+        MyTextData = $OutputInfo
+        TheDataTypeIAm = $InputObject.GetType()
+        MyValue = $InputObject
+    }
+
+}
+
+Format-SomeInput -InputObject "Here is some text"
+Format-SomeInput -InputObject 100
+Format-SomeInput -InputObject (Get-Credential)
